9a20843c0eaa24a935e5c43101e4cfc6710b7ee4
Michał Gromniak <xerun2000@gmail.com>
2023-09-10 01:13:05 +0200

    Pierwszy commit

---
+++ src/main/java/pl/mgromniak/przepisowo/PrzepisowoApplication.java
@@ -0,0 +1,13 @@
+package pl.mgromniak.przepisowo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzepisowoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrzepisowoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mgromniak/przepisowo/PrzepisowoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mgromniak.przepisowo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrzepisowoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
